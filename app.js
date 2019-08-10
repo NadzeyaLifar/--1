8028eb89a7ee4101ca65de8c6d89c905cba59b61
NadzeyaLifar <nadik.lifar@gmail.com>
2019-08-10 09:13:38 +0300

    добавили дз
    добавили дз

---
+++ app.js
@@ -0,0 +1,51 @@
+(function () {
+
+    'use strict'; 
+ 
+ let firstName = prompt('Введите имя');
+ let str = firstName.trim();
+ 
+ let lastName = prompt('Введите фамилию');
+ let str1 = lastName.trim();
+ 
+ while (firstName == '' || firstName == null) {
+     firstName = prompt ('Введите Имя еще раз, данные не корректны');
+ }
+ 
+ while (lastName == '' || lastName == null) {
+     lastName = prompt ('Введите Фамилию еще раз, данные не корректны');
+ }
+ 
+ /*let Gender = confirm('Ваш пол мужской?');
+ 
+ if (Gender === true) {
+     (Gender = 'М');
+ } else {
+     (Gender = 'Ж');
+ }*/
+  
+ let gender = prompt('Введите Ваш пол М или Ж');
+ 
+ while (!(gender.toUpperCase() == 'М' || gender.toUpperCase() == 'Ж')) {
+     gender = prompt ('Введите Введите пол еще раз, данные не корректны'); 
+ }
+ 
+ let age = +prompt('Введите Ваш возраст в годах');//возраст преобразуем в число
+ let retired;
+ 
+ while (typeof (age) != 'number' || age == null || age <= 0) {
+     age = +prompt ('Введите Возраст еще раз, данные не корректны');
+ }
+ 
+ if ((gender.toUpperCase() == 'M' && age >= 63) || (gender.toUpperCase() == 'Ж' && age >=58)) {
+     retired = 'да';
+ }
+     else {
+     retired = 'нет';
+     }
+  
+ alert ('Полное имя:' + ' ' + str + ' ' + str1 + '\n'
+         + 'Пол:' + ' ' + gender.toUpperCase() + '\n'
+         + 'На пенсии:' + ' ' + retired);
+ 
+ }()); 
